bba50e94ddda55b4ac4e6b4bd9ab512d9c24fc2c
amitkumar-29 <tcsamitkumar29@gmail.com>
2021-08-16 09:06:49 +0000

    Log4j added new

--- src/main/java/com/tcs/jbc/demo/JDBCDemo.java
+++ src/main/java/com/tcs/jbc/demo/JDBCDemo.java
@@ -6,8 +6,14 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
 public class JDBCDemo {
 public static void main(String[] args) {
+	Logger logger = LogManager.getLogger(JDBCDemo.class);
+
 	String DB_URL="jdbc:mysql://localhost/practice";
 	String DB_USER="root";
 	String DB_PASSWORD="Nuvelabs123$";
